b4b22c6d8f42335f88a8a6659df4dca8a2729cb4
Sylvain CHAU <sylvain.chau@ensiie.eu>
2025-04-10 18:07:07 +0200

    copy

---
+++ src/board.c
@@ -0,0 +1,205 @@
+#include "../include/board.h"
+#include "../include/card.h"
+#include "../include/player.h"
+
+board create_board() {
+    board b = malloc(sizeof(*b));
+    b->team1.team_id = -1;
+    b->team2.team_id = -1;
+    b->team1.score = 0;
+    b->team2.score = 0;
+    b->team1.player1 = NULL;
+    b->team1.player2 = NULL;
+    b->team2.player1 = NULL;
+    b->team2.player2 = NULL;
+    b->out_of_game_cards = NULL; // We make space for it later
+    return b;
+}
+
+void free_board(board b) {
+    free(b->out_of_game_cards);
+    free(b);
+}
+
+void add_team(board b) {
+    if (b->team1.team_id == -1) {
+        b->team1.team_id = 0;
+    }
+
+    else if (b->team2.team_id == -1) {
+        b->team2.team_id = 1;
+    }
+    else {
+        printf("Error: too many teams\n");
+    }
+}
+
+int get_number_of_teams(board b) {
+    int count = 0;
+    if (b->team1.team_id != -1) {
+        count++;
+    }
+    if (b->team2.team_id != -1) {
+        count++;
+    }
+    return count;
+}
+
+void add_player_to_team(board b, int team_id, player player) {
+    if (b->team1.team_id == team_id) {
+        if (b->team1.player1 == NULL) {
+            b->team1.player1 = player;
+        }
+        else if (b->team1.player2 == NULL) {
+            b->team1.player2 = player;
+        }
+        else {
+            printf("Error: too many players\n");
+        }
+    }
+    
+    if (b->team2.team_id == team_id) {
+        if (b->team2.player1 == NULL) {
+            b->team2.player1 = player;
+        }
+        else if (b->team2.player2 == NULL) {
+            b->team2.player2 = player;
+        }
+        else {
+            printf("Error: too many players\n");
+        }
+    }
+
+    if(!(b->team1.team_id == team_id) && !(b->team2.team_id == team_id)) {
+        printf("Error: wrong team id\n");
+    }
+
+}
+
+int get_number_of_players_in_team(board b, int team_id) {
+    int count = 0;
+    if (b->team1.team_id == team_id) {
+        if (b->team1.player1 != NULL) {
+            count++;
+        }
+        if (b->team1.player2 != NULL) {
+            count++;
+        }
+    }
+    else if (b->team2.team_id == team_id) {
+        if (b->team2.player1 != NULL) {
+            count++;
+        }
+        if (b->team2.player2 != NULL) {
+            count++;
+        }
+    }
+    return count;
+}
+
+player get_player(board b, int team_id, int player_index) {
+    if (b->team1.team_id == team_id) {
+        if (player_index == 0) {
+            return b->team1.player1;
+        }
+        else if (player_index == 1) {
+            return b->team1.player2;
+        }
+        else {
+            printf("Error: wrong player index\n");
+        }
+    }
+    else if (b->team2.team_id == team_id) {
+        if (player_index == 0) {
+            return b->team2.player1;
+        }
+        else if (player_index == 1) {
+            return b->team2.player2;
+        }
+        else {
+            printf("Error: wrong player index\n");
+        }
+    }
+    else {
+        printf("Error: wrong team id\n");
+    }
+    printf("Unknown error\n");
+    return NULL;
+}
+
+int get_score_of_team(board b, int team_id) {
+    if (b->team1.team_id == team_id) {
+        return b->team1.score;
+    }
+    else if (b->team2.team_id == team_id) {
+        return b->team2.score;
+    }
+    else {
+        printf("Error: wrong team id\n");
+    }
+    printf("Unknown error\n");
+    return -1;
+}
+
+void set_score_of_team(board b, int team_id, int score) {
+    if (b->team1.team_id == team_id) {
+        b->team1.score = score;
+    }
+    else if (b->team2.team_id == team_id) {
+        b->team2.score = score;
+    }
+    else {
+        printf("Error: wrong team id\n");
+    }
+    printf("Unknown error\n");
+}
+
+void add_out_of_game_card(board b, card c) {
+    if (b->out_of_game_cards == NULL) {
+        b->out_of_game_cards = malloc(sizeof(card));
+        b->out_of_game_cards[0] = c;
+    }
+    else {
+        int i = 0;
+        while (b->out_of_game_cards[i] != NULL) {
+            i++;
+        }
+        b->out_of_game_cards = malloc(sizeof(card));
+        b->out_of_game_cards[i] = c;
+    }
+}
+
+int get_number_of_out_of_game_cards(board b) {
+    int count = 0;
+    if (b->out_of_game_cards != NULL) {
+        while (b->out_of_game_cards[count] != NULL) {
+            count++;
+        }
+    }
+    return count;
+}
+
+card get_out_of_game_card(board b, int card_index) {
+    if (b->out_of_game_cards != NULL) {
+        return b->out_of_game_cards[card_index];
+    }
+    else {
+        printf("Error: no out of game cards\n");
+    }
+    printf("Unknown error\n");
+    return NULL;
+}
+
+void remove_out_of_game_card(board b, card c) {
+    if (b->out_of_game_cards != NULL) {
+        int i = 0;
+        while (b->out_of_game_cards[i] != c) {
+            i++;
+        }
+        b->out_of_game_cards[i] = NULL;
+    }
+    else {
+        printf("Error: no out of game cards\n");
+    }
+    printf("Unknown error\n");
+}
---
+++ src/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>              
+#include <stdlib.h>
+#include "../include/board.h"
+#include "../include/card.h"
+#include "../include/player.h"
+#include "../include/interface.h"
+
+#define NB_TEAMS 2
+#define NB_PLAYERS_TEAM 2
+#define NB_ROUNDS 3
+#define NB_CARDS 20
+
+int main() {
+
+    /* Initialisation du plateau */
+    display_message("Le jeu va commencer \n");
+
+    board b = create_board();
+    
+    /* Ajout des équipes et des joueuses */
+    int all_card_id[NB_CARDS];
+    int count = 0;
+    
+    card* card_tot = malloc(20 * sizeof(card));
+
+    for (int i = 0; i < NB_TEAMS; i++) {
+        add_team(b);
+        for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+            player p = create_player();
+            add_player_to_team(b, i, p);
+
+            /* Distribution des cartes */
+
+            for (int k = 0; k < 5; k++) {
+                card c = create_card();
+                set_value(c, (k + 1)); 
+                card_tot[count] = c;
+                all_card_id[count] = get_card_id(c);
+                count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < NB_TEAMS; i++) {
+        for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+            for (int k = 0; k < 5; k++) {
+                int random_index = rand() % (NB_CARDS - k);
+                player p = get_player(b, i, j);
+                card c = card_tot[random_index];
+                add_card_to_hand(p, c);
+            }
+        }
+    }
+
+    /* Déroulement des tours */
+
+    for (int round = 0; round < NB_ROUNDS; round++) {
+        display_board(b);
+        
+        /* Phase de pari */
+
+        display_message("Le pari à noter est \"Victoire\" ou \"Défaite\"\n\n");
+
+        for (int i = 0; i < NB_TEAMS; i++) {
+            for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+                player p = get_player(b, i, j);
+                int gamble = ask_gamble(p);
+                set_slate(p, gamble);
+            }
+        }
+        
+        /* Phase de jeu */
+
+        int team_scores[NB_TEAMS] = {0, 0};
+
+        for (int i = 0; i < NB_TEAMS; i++) {
+            for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+                player p = get_player(b, i, j);
+                int nb_cards = ask_number_of_played_cards(p);
+                card* played_cards = malloc(nb_cards * sizeof(card));
+                for (int k = 0; k < nb_cards; k++) {
+                    card c = ask_card(p);
+                    play_card(p, c);
+                    played_cards[k] = c;
+                    team_scores[i] += get_value(c);
+                }
+
+                for (int k = 0; k < nb_cards; k++) {
+                    remove_card_from_hand(p, played_cards[k]);
+                }
+
+            }
+        }
+        
+        /* Détermination du gagnant du tour */
+
+        int max_score = 0;
+        int winning_team = 1;
+
+        if(team_scores[0] == team_scores[1]){
+            display_message("Il y a égalité");
+        }
+
+        for (int i = 0; i < NB_TEAMS; i++) {
+            if (team_scores[i] > max_score) {
+                max_score = team_scores[i];
+                winning_team = i;
+            }
+        }
+        
+        /* Attribution des points */
+
+        for (int i = 0; i < NB_TEAMS; i++) {
+            for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+                player p = get_player(b, i, j);
+                int gamble = get_slate(p);
+                if (winning_team == i && gamble == 1) {
+                    set_score_of_team(b, i, get_score_of_team(b, i) + 1);
+                } else if (winning_team != i && gamble == 0) {
+                    set_score_of_team(b, i, get_score_of_team(b, i) + 1);
+                }
+            }
+        }
+    }
+    
+    /* Fin du jeu */
+    display_end_game(b);
+    
+    /* Libération de la mémoire  */
+    for (int i = 0; i < NB_TEAMS; i++) {
+        for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+            player p = get_player(b, i, j);
+
+            free_player(p);
+        }
+    }
+
+    for(int i = 0; i < NB_CARDS; i++){
+        card c = get_card_by_id(all_card_id[i]);
+        free_card(c);
+    }
+
+    free_board(b);
+    
+    return 0;
+}
