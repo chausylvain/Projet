224f9f943f16ab07711cb1aa527cf8d54d9ca696
ewen_expuesto <ewen.expuesto@gmail.com>
2025-04-22 10:17:41 +0200

    Corrected a few errors in theoretical_study

--- theoretical_study/probability_of_hand_empiric.h
+++ theoretical_study/probability_of_hand_empiric.h
@@ -8,4 +8,12 @@ typedef struct {
 
 tirage distrib(int * pioche);
 
+tirage main_J1 ();
+
+double P_approx_1_main(int i,int j, int n);
+
+double * P_approx_1(int n);
+
+double P(tirage t);
+
 #endif
