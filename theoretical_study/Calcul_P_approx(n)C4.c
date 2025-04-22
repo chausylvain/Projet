a72fdaf3a0ffe96cb129e60860f756f0e090f7e2
MariePrezioso <marieplycee@gmail.com>
2025-04-22 12:23:23 +0200

    Add files via upload

---
+++ theoretical_study/Calcul_P_approx(n)C4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tache_c3.h"
+
+void hist_loi_empirique_C4(int n)
+{
+    FILE *f = fopen("P_approx_1.csv","w"); // ouverture/créée fichier en mode écriture
+    if(f == NULL) // vérifie que le fichier s'est bien ouvert
+    {
+        perror("Erreur lors de l'ouverture du fichier");
+        return 1;
+    }
+    fprintf(f," C¹_1,C¹_2,P_approx_1(n)\n"); // Ligne pour les titres des colonnes
+    double *tab = P_approx_1(n); //stock les valeurs de P_approx_1(n)
+    int k =0;
+    while(tab[k]<6)
+    {
+        for (int i=0; i<4; i++)
+        {
+            for(int j=i; j<4; j++)
+            {
+                fprintf(f,"%d,%d,%f",i,j, tab[k] );
+                k++;
+            }
+        }
+    }
+    fclose(f);
+}
+
+void hist_loi_theorique_C4()
+{
+    FILE *f = fopen("loi_theorique.csv","w"); // ouverture/créée fichier en mode écriture
+    if(f == NULL) // vérifie que le fichier s'eest bien ouvert
+    {
+        perror("Erreur lors de l'ouverture du fichier");
+        return 1;
+    }
+    fprintf(f," C¹_1,C¹_2,P(C¹_1,C¹_2)\n"); // Ligne pour les titres des colonnes
+    for(int j=1; j<4; j++)
+    {
+        for (int i=1; i<=j; i++)
+        {
+            tirage t = {i,j};
+            fprintf(f, "%d,%d,%d\n", i,j,P(t));
+        }
+    }
+    fclose(f);
+}
+
+int main()
+{
+    int n = 1000;
+    hist_loi_empirique_C4(n);
+    hist_loi_theorique_C4();
+}
